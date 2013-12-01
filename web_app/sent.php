1debdb300e6bf09154c865f4796cb8a41976897f
malcjohn <cincenko@seznam.cz>
2013-12-02 00:15:22 +0100

    update

--- web_app/sent.php
+++ web_app/sent.php
@@ -3,22 +3,20 @@
     <head>
         <title>Android WebApp</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta charset="utf-8">
+        <meta http-equiv="content-type" content="text/html; charset=utf-8">
         <meta name="description" content="Android Web App, see our GitHub">
-        <link rel="stylesheet" href="http://bootswatch.com/readable/bootstrap.min.css"> <!-- http://bootswatch.com/ -->
-        <link rel="stylesheet" href="css/bootstrap.min.css"> 
-        <link rel="stylesheet" href="css/jquery.pnotify.default.css"> 
+        <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css" rel="stylesheet">
         <link rel="stylesheet" href="css/styles.css">
-        <script src="http://code.jquery.com/jquery-1.10.1.min.js" ></script> 
-        <script src="js/bootstrap.min.js"></script>
+        <script src="http://code.jquery.com/jquery-2.0.3.min.js" ></script> 
+        <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.2/js/bootstrap.min.js"></script>
         <script src="js/maplace.min.js"></script>
-        <script src="js/jquery.pnotify.min.js"></script>
-        <!-- <link rel="shortcut icon" href="img/favicon.ico"> -->
+        <link rel="shortcut icon" href="images/icon.png"> 
     </head> 
 
     <?php      
-    //Connection to the server
-                $conn = mysqli_connect('127.0.0.1', 'komentovaneud003', 'android_reut', 'komentovaneudalosticz02') or die("privni bla");
+                header('Content-type: text/html; charset=utf-8');
+                //Connection to the server
+                $conn = mysqli_connect('127.0.0.1', 'komentovaneud003', 'android_reut', 'komentovaneudalosticz02') or die("Failed #1");
                 // if some error 
                 if (mysqli_connect_errno()){
                     echo "Failed to connect to MySQL: " . mysqli_connect_error();
@@ -26,44 +24,76 @@
 
                 // http://diskuse.jakpsatweb.cz/?action=vthread&forum=9&topic=145971 We need isset(), but also
                 // http://stackoverflow.com/questions/19465195/mysql-stores-only-1-as-a-value-no-matter-what-numbers-i-submit-in-the-form
-                $longti = $_POST['inputLongitude']; 
-                $lati = $_POST['inputLatitude'];
-                $titel = $_POST['inputTitel'];
-                $snippets = $_POST['inputSnippets'];
                 $safe_lati = mysqli_real_escape_string($conn, $_POST['inputLatitude']);
                 $safe_longti = mysqli_real_escape_string($conn, $_POST['inputLongitude']);
                 $safe_titel = mysqli_real_escape_string($conn, $_POST['inputTitel']);
                 $safe_snippets = mysqli_real_escape_string($conn, $_POST['inputSnippets']);
-                echo $safe_titel;
-                echo $safe_snippets;
-                $sql = "INSERT INTO `komentovaneudalosticz02`(`key_prim`, `lati`, `longti`, `titel`, `snippets`)
-                VALUES (`key_prim`,$safe_lati,$safe_longti,$safe_titel,$safe_snippets)";
+                // saves varibles with help of SQL
+                $sql = "INSERT INTO `komentovaneudalosticz022`(`key_prim`, `lati`, `longti`,`titel`, `snippets`)
+                VALUES (`key_prim`,$safe_lati,$safe_longti,'$safe_titel','$safe_snippets')";
                 mysqli_query($conn,$sql);
     ?>
 
     <body>
+        <a href="http://projekty.komentovaneudalosti.cz/android">
+            <img src="images/Logo.png" class="img-rounded" style="height:100px; wight:150px;"></img>
+        </a>
+
         <div class="row">
-            <div class="col-xs-12 col-md-8">    
+            <div class="col-xs-6">    
                 <table class="table table-striped">
                     <thead>
                         <tr>
                             <th>Primary Key in DB</th>
                             <th>Longitude</th>
                             <th>Latitude</th>
-                            <th>Json</th>
                             <th>Titel</th>
                             <th>Snippets</th>
                         </tr>
                     </thead>
                     <tbody>
                         <?php
+                           // header('Content-type: text/html; charset=utf-8');
                             $cn = mysqli_connect('127.0.0.1', 'komentovaneud003', 'android_reut', 'komentovaneudalosticz02') or die("Failed #2");
-                            // It prints the results from DB through a SQL request
-                            $q = "SELECT `key_prim`, `lati`, `longti`, `titel`, `snippets` FROM `komentovaneudalosticz02`";
+                            // It fetches results from DB through a SQL request
+                            $q = "SELECT `key_prim`, `lati`, `longti`, `titel`, `snippets` FROM `komentovaneudalosticz022`";
                             $results = mysqli_query($cn, $q) or die(mysqli_error($cn));
                             $json = array();
-                            // It fetches results from db and stores them in a row
+                            // It fetches results from db and stores them in a row -> prints them
                             // http://stackoverflow.com/questions/2974011/while-row-mysql-fetch-arrayresult-how-many-loops-are-being-performed
+                            while ($row = mysqli_fetch_array($results)){
+                                echo "<tr>
+                                <td>".$row['key_prim']."</td>
+                                <td>".$row['longti']."</td>
+                                <td>".$row['lati']."</td>
+                                <td>".$row['titel']."</td>
+                                <td>".$row['snippets']."</td>
+                                </tr>";                             
+                            }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+            <div class="col-xs-6">
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>Json</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                            // To delete 2 
+                            // header('Content-type: text/html; charset=utf-8');
+                            // $cn = mysqli_connect('127.0.0.1', 'komentovaneud003', 'android_reut', 'komentovaneudalosticz02') or die("Failed #3");
+                            // It prints the results from DB through a SQL request
+                            $q = "SELECT `key_prim`, `lati`, `longti`, `titel`, `snippets` FROM `komentovaneudalosticz022`";
+                            $results = mysqli_query($cn, $q) or die(mysqli_error($cn));
+                            // no need probably
+                            $json = array();
+                            // http://www.akawebdesign.com/2009/08/18/json-formatting-in-php/
+                            $jsonResponse = array("data" => array());
+
                             while ($row = mysqli_fetch_array($results)){
                                 $json["key_prim"] = $row["key_prim"];
                                 $json["lati"] = $row["lati"];
@@ -71,50 +101,46 @@
                                 $json["titel"] = $row["titel"];
                                 $json["snippets"] = $row["snippets"];
 
-                                $text_json = json_encode($json, JSON_PRETTY_PRINT);
-                                // create data array
+                                // create data array    
                                 $named_array = array(
-                                    "data" => array(
-                                        array(
                                            "key_prim" => $row['key_prim'],
                                            "lati" => $row['lati'],
                                            "longti" => $row['longti'], 
                                            "titel" => $row['titel'],
-                                           "snippets" => $row['snippets']
-                                        ),                                         
-                                        // array(
-                                        //     "lati" => $row['lati']
-                                        // ),
-                                        // array(
-                                        //     "longti" => $row['longti']
-                                        // ),
-                                    )
+                                           "snippets" => $row['snippets'],
+                                           "altitude" => "0.0"
                                 );
-                                $named_array_json = json_encode($named_array, JSON_PRETTY_PRINT);
+                                // encode named array in a pretty form and save
+                                $named_array_json = json_encode($named_array);
+                                // behave such as stack; pushes the passed variables onto the end 
+                                // http://php.net/manual/en/function.array-push.php
+                                array_push($jsonResponse['data'], $named_array);
 
-                                echo "<tr>
-                                <td>".$row['key_prim']."</td>
-                                <td>".$row['longti']."</td>
-                                <td>".$row['lati']."</td>
-                                <td>".$row['titel']."</td>
-                                <td>".$row['snippets']."</td>
-                                <td><pre>".$text_json."</pre></td>
-                                <td><pre>".$named_array_json."</pre></td>
-                                </tr>";
-
-                				$fp = fopen('json.json', 'w');
-                				fwrite($fp, $named_array_json);
-                				fclose($fp);                                
                             }
+                            $json_saved_response = json_encode($jsonResponse, JSON_PRETTY_PRINT);
+                            // print named array json in pritty. 
+                            echo "<td><pre>".json_encode($jsonResponse, JSON_PRETTY_PRINT)."</pre></td>";
+                            // create json.json file which is generated on the fly from json saved variable
+                            $fp = fopen('json.json', 'w');
+                            fwrite($fp, $json_saved_response);
+                            fclose($fp); 
                         ?>
-                    </tbody>
+                    </tbody>    
                 </table>
             </div>
-
-            <div class="col-xs-6 col-md-4">
-
-            </div>
         </div>
+
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-11552074-6', 'komentovaneudalosti.cz');
+          ga('send', 'pageview');
+
+        </script>
+
     </body>
 </html>
 
